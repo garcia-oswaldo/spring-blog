b5cb114a3cf22071f0c65995abec501ff41a51e7
oswaldo garcia <garcia.oswaldo1000@gmail.com>
2021-10-01 11:00:08 -0500

    finsished the delete function

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -18,61 +18,69 @@ public class PostController {
 
     public PostController(PostRepository postDao, UserRepository userDao, EmailService emailService) {
         this.postDao = postDao;
-        this.userDao= userDao;
-        this.emailService= emailService;
+        this.userDao = userDao;
+        this.emailService = emailService;
     }
 
     @GetMapping("/posts")
     public String showPosts(Model model) {
-        model.addAttribute("posts",postDao.findAll());
+        model.addAttribute("posts", postDao.findAll());
         return "posts";
     }
 
     @GetMapping("/posts/{id}")
     @ResponseBody
     public String showOnePost(@PathVariable long id, Model model) {
-        Post post =postDao.getById(id);
+        Post post = postDao.getById(id);
         return "post/show";
     }
 
     @GetMapping("/create")
     public String showCreatePostForm(Model model) {
-         //we need to pass empty model to form
+        //we need to pass empty model to form
 //        Post postToEdit =postDao.getById(id);
         model.addAttribute("post", new Post());
 
         return "create";
     }
 
-@GetMapping("/post/edit/{id}")
-public String viewEditForm(@PathVariable long id, Model model){
-        Post postToEdit=postDao.getById(id);
+    @GetMapping("/post/edit/{id}")
+    public String viewEditForm(@PathVariable long id, Model model) {
+        Post postToEdit = postDao.getById(id);
         model.addAttribute("postToEdit", postToEdit);
-                return "edit";
-}
+        return "edit";
+    }
+
     @PostMapping("/post/edit/{id}")
-        public String editPost(@PathVariable long id, @ModelAttribute Post postToEdit)
-    {
+    public String editPost(@PathVariable long id, @ModelAttribute Post postToEdit) {
 //            Post post= postDao.getById(id);
 //            post.setTitle(postToEdit.getTitle());
 //            post.setDescription(postToEdit.getDescription());
-        User currentLoggedInUser=(User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User currentLoggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         postToEdit.setOwner(currentLoggedInUser);
-            postToEdit.setId(id);
-            postDao.save(postToEdit);
-         return "redirect:/posts";
-        }
+        postToEdit.setId(id);
+        postDao.save(postToEdit);
+        return "redirect:/posts";
+    }
 
 
     @PostMapping("/create")
     public String createPost(@ModelAttribute Post postToSubmit) {
-        User currentLoggedInUser=(User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User currentLoggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         postToSubmit.setOwner(currentLoggedInUser);
         postDao.save(postToSubmit);
 
-        emailService.prepareAndSend(postToSubmit, "newPost","You created a new Post");
+        emailService.prepareAndSend(postToSubmit, "newPost", "You created a new Post");
 
         return "redirect:/posts";
     }
 
+    @PostMapping("/post/delete/{id}")
+    public String deletePost(@PathVariable long id) {
+        postDao.deleteById(id);
+
+        return "redirect:/posts";
+
+
+    }
 }
